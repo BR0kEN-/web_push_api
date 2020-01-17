a7f4aa9e2554b0c351c6cf23232a6f9e853ae2cb
BR0kEN <BR0kEN@2802285.no-reply.drupal.org>
2020-01-17 23:28:15 +0400

    Issue #3107034 by BR0kEN: Try installing PHP extension on DrupalCI

--- tests/drupalci.provision.sh
+++ tests/drupalci.provision.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+echo "==> Prepare the environment..."
+
 if ! php -m | grep gmp > /dev/null && command -v docker-php-ext-install > /dev/null; then
     echo "==> Installing GMP PHP extension..."
     docker-php-ext-install gmp
